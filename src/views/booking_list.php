e97ea9b9193a8273b7d006203b6f48a0ab4bca5b
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-12 14:29:09 +0200

    booking list integration

---
+++ src/views/booking_list.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Medical Center</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php require 'partials/_navbar.php' ?>
+    
+    <!-- start form -->
+    <div class="container py-5">
+        <h1 class="mb-3">Liste des rendez-vous !</h1>
+
+        <?php for($i = 0; $i < 10; $i++): ?>
+            <div class="card mb-3">
+                <div class="card-header">
+                    12/06/2023 à 14:25
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">NOM Prénom</h5>
+                    <p class="card-text">Email</p>
+                    <p class="card-text">Adresse 1</p>
+                    <p class="card-text">Adresse 2</p>
+                    <p class="card-text">00000 Ville</p>
+                    <a href="#" class="btn btn-primary">Modifier</a>
+                </div>
+            </div>
+        <?php endfor ?>
+    </div>
+    <!-- end form -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
