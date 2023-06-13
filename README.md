47fed0ec6db156bb47e25aca6801bce3d2990fc0
Vincent Sureau <vincentsureau5@gmail.com>
2023-06-13 09:35:40 +0200

    added database configuration

--- README.md
+++ README.md
@@ -17,3 +17,7 @@
 ## MCD
 
 ![MCD](docs/bdd.png)
+
+## Configuration de la base de données
+- copier le fichier de configuration avec `cp config.dist.ini config.ini`
+- remplir les informations de connexion à la base de données
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,20 @@ class HomeController
 
     public function list()
     {
+        $config = parse_ini_file(dirname(__FILE__, 3). '/config.ini');
+        $db_dbname = $config['DB_NAME'];
+        $db_host = $config['DB_HOST'];
+        $db_port = $config['DB_PORT'];
+        $db_user = $config['DB_USER'];
+        $db_pass = $config['DB_PASSWORD'];
+        try {
+            $dbh = new \PDO("mysql:host=$db_host;port=$db_port;dbname=$db_dbname", $db_user, $db_pass,array(
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            ));
+        } catch (\PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
         require dirname(__FILE__, 2) . '/views/booking_list.php';
     }
 }
